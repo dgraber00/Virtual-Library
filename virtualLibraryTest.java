a15d94d5c71cd4d4d9d87fcb079569b5d6c3d4a5
jtowner2 <107655468+jtowner2@users.noreply.github.com>
2023-11-13 18:45:14 -0500

    Update virtualLibraryTest.java

--- virtualLibraryTest.java
+++ virtualLibraryTest.java
@@ -1,9 +1,37 @@
+public class VirtualLibraryTest {
 
-public class virtualLibraryTest {
+    public static void main(String[] args) {
+        // Initialize the virtual library
+        VirtualLibrary myLibrary = new VirtualLibrary();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Create user accounts
+        LibraryUserAccount userAccount1 = new LibraryUserAccount(0, "Jane Doe", 101);
+        LibraryUserAccount userAccount2 = new LibraryUserAccount(1, "John Smith", 102);
 
-	}
+        // Add user accounts to the library
+        myLibrary.addUserAccount(userAccount1);
+        myLibrary.addUserAccount(userAccount2);
 
+        // Display all user accounts
+        System.out.println("Displaying all user accounts after adding:");
+        myLibrary.displayUserAccounts();
+
+        // Simulate checking out a book for Jane Doe
+        userAccount1.setNumberOfBooks(userAccount1.getNumberOfBooks() + 1);
+        System.out.println("Jane Doe checks out a book. Total books now: " + userAccount1.getNumberOfBooks());
+
+        // Simulate returning a book for John Smith
+        userAccount2.setNumberOfBooks(userAccount2.getNumberOfBooks() - 1);
+        System.out.println("John Smith returns a book. Total books now: " + userAccount2.getNumberOfBooks());
+
+        // Display all user accounts after checkout and return
+        System.out.println("\nDisplaying all user accounts after checkout and return:");
+        myLibrary.displayUserAccounts();
+
+        // Test releasedBooks method for a user reaching the limit
+        for (int i = 0; i < 9; i++) {
+            userAccount1.releasedBooks(userAccount1.getNumberOfBooks());
+        }
+        System.out.println("After attempting to checkout multiple books, Jane Doe has: " + userAccount1.getNumberOfBooks() + " books.");
+    }
 }
